b13e7bc7e6d72024c0ba80c3f993163b787c3a89
Mohamed Elsharnouby <sharnoby3@gmail.com>
2021-12-06 22:29:20 +0000

    Initial commit

---
+++ Main.hs
@@ -0,0 +1,21 @@
+import Day1
+import Day2
+import Day3
+import Day4
+import Day5
+import Day6
+
+main = do
+  -- putStrLn $ show day1p1
+  -- putStrLn $ show day1p2
+  -- putStrLn $ show day2p1
+  -- putStrLn $ show day2p2
+  -- putStrLn $ show day3p1
+  -- putStrLn $ show day3p2
+  -- putStrLn $ show day4p1
+  -- putStrLn $ show day4p2
+  -- putStrLn $ show day5p1
+  -- putStrLn $ show day5p2
+  putStrLn $ show day6p1
+  putStrLn $ show day6p2
+
