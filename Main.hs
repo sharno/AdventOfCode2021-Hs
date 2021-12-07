c36d4dacc86b4c1d67b13419ebdd9e7e2f2fb8d4
Mohamed Elsharnouby <sharnoby3@gmail.com>
2021-12-07 14:23:46 +0000

    Solve Day 7

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@ import Day3
 import Day4
 import Day5
 import Day6
+import Day7
 
 main = do
   -- putStrLn $ show day1p1
@@ -16,6 +17,8 @@ main = do
   -- putStrLn $ show day4p2
   -- putStrLn $ show day5p1
   -- putStrLn $ show day5p2
-  putStrLn $ show day6p1
-  putStrLn $ show day6p2
+  -- putStrLn $ show day6p1
+  -- putStrLn $ show day6p1
+  -- putStrLn $ show day7p1
+  -- putStrLn $ show day7p2
 
